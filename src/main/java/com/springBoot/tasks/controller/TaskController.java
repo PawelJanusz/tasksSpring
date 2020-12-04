821f7b153db1f04e3ff11ddb7e60a506a84e7b3a
Pawel Janusz <marek80k@wp.pl>
2020-12-04 21:13:56 +0100

    configuration

---
+++ src/main/java/com/springBoot/tasks/controller/TaskController.java
@@ -0,0 +1,64 @@
+package com.springBoot.tasks.controller;
+
+import com.springBoot.tasks.model.Task;
+import com.springBoot.tasks.model.SqlTaskRepository;
+import com.springBoot.tasks.model.TaskRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate){
+        Task result = repository.save(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
+
+    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})//wchodzimy na taski tylko wtedy gdy nie ma 3 zapisanych parametrów
+    ResponseEntity<List<Task>> readAllTasks(){
+        logger.warn("Exposing all the task");
+        return ResponseEntity.ok(repository.findAll()); //lista otrzymanych tasków
+    }
+
+    @GetMapping("/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page){
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
+
+    @GetMapping("tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
+        if (!repository.existsById(id)){
+            ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
---
+++ src/test/java/com/springBoot/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
